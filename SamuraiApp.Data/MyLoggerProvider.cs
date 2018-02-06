03f79a09cf3288f3a1d7f9cc1786f51246981a2e
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-06 14:17:40 +0200

    Inserting Simple Objects and Loging

---
+++ SamuraiApp.Data/MyLoggerProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SamuraiApp.Data
+{
+    public class MyLoggerProvider : ILoggerProvider
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new MyLogger();
+        }
+
+        private class MyLogger : ILogger
+        {
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                //File.AppendAllText(@"C:\temp\log.txt", formatter(state, exception));
+                Console.WriteLine(formatter(state, exception));
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+        }
+    }
+}
--- SomeUI/Program.cs
+++ SomeUI/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace SomeUI
 {
@@ -10,6 +14,19 @@ namespace SomeUI
     {
         static void Main(string[] args)
         {
+            InsertSamurai();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai {Name = "Julie"};
+
+            using (var context = new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
         }
     }
 }
